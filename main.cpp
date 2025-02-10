48763b81d854fc8d965341519ddd8aeed710326a
Mru1726 <202401122@daiict.ac.in>
2025-02-10 17:30:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-//Partial code
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int GRID_SIZE = 20;
+const char SNAKE_CHAR = 'O';
+const char FOOD_CHAR = '*';
+const char EMPTY_CHAR = ' ';
+
+enum Direction { UP, DOWN, LEFT, RIGHT };
+
+class Snake {
+public:
+    deque<pai<int, int>> body;
+    Direction dir;
